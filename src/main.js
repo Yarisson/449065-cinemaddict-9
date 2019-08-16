c5b2063e69e444c7a0a9d350ceeeb52812fdef18
Yarisson18 <gemini8811@gmail.com>
2019-08-16 21:28:20 +0300

    правки по замечаниям от наставника

--- src/main.js
+++ src/main.js
@@ -3,42 +3,69 @@ import {createMenuTemplate} from './components/menu.js';
 import {createProfileRatingTemplate} from './components/rating.js';
 import {createFilmsWrapperTemplate} from './components/films.js';
 import {createFilmCardTemplate} from './components/film.js';
-import {createShowMoreButtonTemplate} from './components/showmore.js';
+import {createShowMoreButtonTemplate} from './components/show-more.js';
 import {createFilmPopupTemplate} from './components/popup.js';
-// функция рендера
+
+/**
+ * Функция рендера
+ */
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
-// константы
+
+/**
+ * Константы
+ */
+const Number_Films_Card = 5;
+const Number_Extra_Films_Card = 2;
+
+/**
+ * Функция для рендера дополнительных карточек в блоки filmsListsExtra
+ */
+const renderFilmsExtraLists = () => {
+  filmsListsExtra.forEach(function (item) {
+    return Array(Number_Extra_Films_Card).fill(``).forEach(() => render(item.querySelector(`.films-list__container`), createFilmCardTemplate(), `beforeend`));
+  });
+};
+
+/**
+ * Поиск элементов в ДОМ-API
+ */
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
-const numberFilmsCard = 5;
-const numberExtraFilmsCard = 2;
 
-// Отрисовка блоков в шапку
+/**
+ * Отрисовка блоков в шапку
+ */
 render(header, createSearchTemplate(), `beforeend`);
 render(header, createProfileRatingTemplate(), `beforeend`);
 
-// Отрисовка меню
+/**
+ * Отрисовка меню
+ */
 render(main, createMenuTemplate(), `beforeend`);
 
-// Отрисовка оберток для фильмов
+/**
+ * Отрисовка оберток для фильмов
+ */
 render(main, createFilmsWrapperTemplate(), `beforeend`);
 
+/**
+ * Поиск элементов в ДОМ-API из отрисованных оберток фильмов
+ */
 const filmsList = document.querySelector(`.films-list`);
 const filmsListContainer = filmsList.querySelector(`.films-list__container`);
 const filmsListsExtra = document.querySelectorAll(`.films-list--extra`);
 
-// функция для рендера дополнительных карточек в блоки filmsListsExtra
-const renderFilmsExtraLists = () => {
-  filmsListsExtra.forEach(function (item) {
-    return Array(numberExtraFilmsCard).fill(``).forEach(() => render(item.querySelector(`.films-list__container`), createFilmCardTemplate(), `beforeend`));
-  });
-};
-
-// Отрисовка карточек
-new Array(numberFilmsCard).fill(``).forEach(() => render(filmsListContainer, createFilmCardTemplate(), `beforeend`));
+/**
+ * Отрисовка карточек
+ */
+new Array(Number_Films_Card).fill(``).forEach(() => render(filmsListContainer, createFilmCardTemplate(), `beforeend`));
 renderFilmsExtraLists();
-// Отрисовка кнопки и попапа
+
+/**
+ * Отрисовка кнопки и попапа
+ */
 render(filmsList, createShowMoreButtonTemplate(), `beforeend`);
 render(main, createFilmPopupTemplate(), `beforeend`);
