1d9de2f86b44dd2eea0d717035875f80957990e6
Yarisson18 <gemini8811@gmail.com>
2019-08-17 22:40:19 +0300

    правки по замечаниям в имена констант и комментарии

--- src/main.js
+++ src/main.js
@@ -9,63 +9,46 @@ import {createFilmPopupTemplate} from './components/popup.js';
 /**
  * Функция рендера
  */
-
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-/**
- * Константы
- */
-const Number_Films_Card = 5;
-const Number_Extra_Films_Card = 2;
+// Константы
+const NUMBER_FILMS_CARD = 5;
+const NUMBER_EXTRA_FILMS_CARD = 2;
 
 /**
  * Функция для рендера дополнительных карточек в блоки filmsListsExtra
  */
 const renderFilmsExtraLists = () => {
   filmsListsExtra.forEach(function (item) {
-    return Array(Number_Extra_Films_Card).fill(``).forEach(() => render(item.querySelector(`.films-list__container`), createFilmCardTemplate(), `beforeend`));
+    return Array(NUMBER_EXTRA_FILMS_CARD).fill(``).forEach(() => render(item.querySelector(`.films-list__container`), createFilmCardTemplate(), `beforeend`));
   });
 };
 
-/**
- * Поиск элементов в ДОМ-API
- */
+// Поиск элементов в ДОМ-API
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
 
-/**
- * Отрисовка блоков в шапку
- */
+// Отрисовка блоков в шапку
 render(header, createSearchTemplate(), `beforeend`);
 render(header, createProfileRatingTemplate(), `beforeend`);
 
-/**
- * Отрисовка меню
- */
+// Отрисовка меню
 render(main, createMenuTemplate(), `beforeend`);
 
-/**
- * Отрисовка оберток для фильмов
- */
+// Отрисовка оберток для фильмов
 render(main, createFilmsWrapperTemplate(), `beforeend`);
 
-/**
- * Поиск элементов в ДОМ-API из отрисованных оберток фильмов
- */
+// Поиск элементов в ДОМ-API из отрисованных оберток фильмов
 const filmsList = document.querySelector(`.films-list`);
 const filmsListContainer = filmsList.querySelector(`.films-list__container`);
 const filmsListsExtra = document.querySelectorAll(`.films-list--extra`);
 
-/**
- * Отрисовка карточек
- */
-new Array(Number_Films_Card).fill(``).forEach(() => render(filmsListContainer, createFilmCardTemplate(), `beforeend`));
+// Отрисовка карточек
+new Array(NUMBER_FILMS_CARD).fill(``).forEach(() => render(filmsListContainer, createFilmCardTemplate(), `beforeend`));
 renderFilmsExtraLists();
 
-/**
- * Отрисовка кнопки и попапа
- */
+// Отрисовка кнопки и попапа
 render(filmsList, createShowMoreButtonTemplate(), `beforeend`);
 render(main, createFilmPopupTemplate(), `beforeend`);
