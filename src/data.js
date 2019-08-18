683e3ccbbfc92a59577d45c8184e91cafad72548
Yarisson18 <gemini8811@gmail.com>
2019-08-19 02:28:02 +0300

    добавление файла, где хранятся данные и начало добавления этих данных

---
+++ src/data.js
@@ -0,0 +1,41 @@
+const POSTERS_NUMBER = 7;
+
+const TEXT_DESCRIPTION = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  Cras aliquet varius magna, non porta ligula feugiat eget.
+  Fusce tristique felis at fermentum pharetra.
+  Aliquam id orci ut lectus varius viverra.
+  Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.
+  Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.
+  Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.
+  Sed sed nisi sed augue convallis suscipit in sed felis.
+  Aliquam erat volutpat.
+  Nunc fermentum tortor ac porta dapibus.
+  In rutrum ac purus sit amet tempus.`
+];
+
+const getTask = () => ({
+  title: [
+    `Back to the future`,
+    `Star wars`,
+    `Flash gordon`,
+    `Total recall`,
+    `Star gate`,
+    `Doctor who`,
+    `Rocky`,
+    `Major league`,
+    `6 day, 7 night`,
+    `Enemy of the state`,
+    `The Lives of others`,
+    `Gone in sixty seconds`,
+    `The rock`,
+    `Lost`,
+    `Professional`
+  ],
+  poster: {
+    url: `../public/images/posters/*.png`
+  },
+  description: [
+    TEXT_DESCRIPTION.split(`.`),
+  ][Math.floor(Math.random() * 3)],
+});
